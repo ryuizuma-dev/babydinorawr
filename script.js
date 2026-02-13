a61682dc87366374876cf0eee32dc03441dfd171
ryuizuma-dev <ryuizuma@gmail.com>
2026-02-13 22:38:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,26 @@ let slides;
 
 document.addEventListener("DOMContentLoaded", function () {
   slides = document.querySelectorAll(".slide");
-  showSlide(currentSlide);
+
+  // Hide everything except intro
+  document.getElementById("surprise").style.display = "none";
+  slides.forEach(slide => slide.style.display = "none");
 });
+
+function showSurprise() {
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("surprise").style.display = "flex";
+
+  setTimeout(() => {
+    document.getElementById("surprise").style.display = "none";
+    currentSlide = 0;
+    showSlide(currentSlide);
+  }, 3000); // 3 seconds birthday screen
+}
 
 function showSlide(index) {
   slides.forEach((slide, i) => {
-    if (i === index) {
-      slide.style.display = "flex";
-      slide.style.opacity = "1";
-    } else {
-      slide.style.display = "none";
-      slide.style.opacity = "0";
-    }
+    slide.style.display = (i === index) ? "flex" : "none";
   });
 }
 
