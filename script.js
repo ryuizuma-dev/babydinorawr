b936fe4639be01913ff488653d54b4767ee91153
ryuizuma-dev <ryuizuma@gmail.com>
2026-02-13 22:09:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let currentSlide = 0;
-const slides = document.querySelectorAll(".slide");
+let slides;
+
+document.addEventListener("DOMContentLoaded", function () {
+  slides = document.querySelectorAll(".slide");
+  showSlide(currentSlide);
+});
 
 function showSlide(index) {
-  slides.forEach(slide => slide.classList.remove("active"));
-  slides[index].classList.add("active");
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = "flex";
+      slide.style.opacity = "1";
+    } else {
+      slide.style.display = "none";
+      slide.style.opacity = "0";
+    }
+  });
 }
 
 function nextSlide() {
-  currentSlide++;
-  if (currentSlide < slides.length) {
+  if (currentSlide < slides.length - 1) {
+    currentSlide++;
     showSlide(currentSlide);
   }
 }
